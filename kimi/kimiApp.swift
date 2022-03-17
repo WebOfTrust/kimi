537bdc6955368b6dab531f63c1130f7f3cabe561
Kevin Griffin <griffin.kev@gmail.com>
2022-03-17 13:06:02 -0400

    Initial Commit

---
+++ kimi/kimiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kimiApp.swift
+//  kimi
+//
+//  Created by Kevin Griffin on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct kimiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
