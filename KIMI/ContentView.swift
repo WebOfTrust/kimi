19c98c1621bc212a3ca03d14d32045bd4f8882ed
Mijo Kaliger <mijo.kaliger@gmail.com>
2022-04-06 18:11:40 +0200

    Add StyleGuide with styles defined in Zeplin Layout first onboarding screen "Welcome"

--- KIMI/ContentView.swift
+++ KIMI/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack(alignment: .top) {
+                Color(ColorType.background.name)
+                    .ignoresSafeArea(.all, edges: .bottom)
+
+                ContainerView {
+                    VStack(spacing: Spacing.contentSpacing) {
+                        Text("Welcome to KEEP")
+                            .font(Font(uiFont: .headline))
+
+                        Image("welcome")
+
+                        Text("To get started and enter the software, you will utilize either your PIN or Face ID.")
+                            .font(Font(uiFont: .subheadline))
+                            .color(.subheadline)
+                            .fixedSize(horizontal: false, vertical: true)
+
+                        HStack {
+                            Spacer()
+
+                            ActionButton(text: "Continue", background: .primaryAction) {
+
+                            }
+                        }
+
+                    }.padding(Spacing.contentPadding)
+                }
+                .fixedSize(horizontal: false, vertical: true)
+
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("KIMI")
+                        .color(.branding)
+                        .font(Font(uiFont: .navTitle))
+                }
+            }
+        }
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
